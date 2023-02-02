347b31fb9b4318c4bfbf124891eff125f4b94d1f
dronv <dronvyas4454@gmail.com>
2023-02-02 18:31:26 -0500

    first commit

---
+++ sendmsg.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+#syntax(phone number, message, hours, minutes, seconds, True if want to close tab, closing tab after seconds)
+pywhatkit.sendwhatmsg("+1XXXXXXXXXXX", "Hello World!", 17, 38, 15, True, 2)
