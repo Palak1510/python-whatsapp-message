11ad57aed2c552650eea3d62c3c7d9dba57d8af5
Palak1510 <ptadhani@lakeheadu.ca>
2023-02-02 18:49:45 -0500

    changed message

--- sendmsg.py
+++ sendmsg.py
@@ -1,4 +1,4 @@
 import pywhatkit
 
 #syntax(phone number, message, hours, minutes, seconds, True if want to close tab, closing tab after seconds)
-pywhatkit.sendwhatmsg("+1XXXXXXXXXXX", "Hello World!", 17, 38, 15, True, 2)
+pywhatkit.sendwhatmsg("+1XXXXXXXXXXX", "What's up?", 17, 38, 15, True, 2)
